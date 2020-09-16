1f49467fba932a5da4eb2d7c43b52b93e604df31
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-16 13:49:50 -1000

    Added Vitack xcode project

---
+++ Vitack/Vitack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vitack
+//
+//  Created by Jesse Ruiz on 9/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vitack/Vitack/VitackApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitackApp.swift
+//  Vitack
+//
+//  Created by Jesse Ruiz on 9/16/20.
+//
+
+import SwiftUI
+
+@main
+struct VitackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
